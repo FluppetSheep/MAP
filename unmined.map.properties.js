c92d7def91d7e519f74c546ff3497f498a04593d
FluppetSheep <48367791+FluppetSheep@users.noreply.github.com>
2022-11-01 21:38:50 +0000

    1 Nov update

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -6,9 +6,9 @@ var UnminedMapProperties = {
     imageFormat: "jpeg",
     minRegionX: -7,
     minRegionZ: -9,
-    maxRegionX: 16,
+    maxRegionX: 30,
     maxRegionZ: 4,
-    worldName: "Just Survival 10/25/22 20:06:06 - Copy",
+    worldName: "Just Survival 11/01/22 19:01:40 - Copy",
     background: "",
     markers: new Array()
 }
